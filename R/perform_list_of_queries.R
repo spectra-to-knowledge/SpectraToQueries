8e06d557d92010c63a2b25031380cfca79713f31
Adriano Rutz <adriano.rutz@hotmail.com>
2023-12-14 19:31:23 +0100

    improving optimal query search (better combinations)

--- R/perform_list_of_queries.R
+++ R/perform_list_of_queries.R
@@ -9,27 +9,20 @@
 #'
 #' @examples
 perform_list_of_queries <- function(index, ions_list, spectra) {
-  message("Extract the fragments of the list.")
-  frags <-
-    ions_list[[index]][ions_list[[index]] |> grepl(pattern = "frag")] |>
-    gsub(pattern = "_frag", replacement = "") |>
-    as.numeric()
-
-  message("Extract the neutral losses of the list.")
-  nls <-
-    ions_list[[index]][ions_list[[index]] |> grepl(pattern = "nl")] |>
-    gsub(pattern = "_nl", replacement = "") |>
-    as.numeric()
-
-  message("Perform the query.")
-  matching_spectra <- spectra |>
-    perform_query(frags = frags, nls = nls)
-
-  message("Return the corresponding tidytable.")
   return(tidytable::tidytable(
     target = names(ions_list)[index],
     value = c(
-      matching_spectra |>
+      spectra |>
+        perform_query(
+          frags = ions_list[[index]][ions_list[[index]] |>
+            grepl(pattern = "frag")] |>
+            gsub(pattern = "_frag", replacement = "") |>
+            as.numeric(),
+          nls = ions_list[[index]][ions_list[[index]] |>
+            grepl(pattern = "nl")] |>
+            gsub(pattern = "_nl", replacement = "") |>
+            as.numeric()
+        ) |>
         Spectra::spectraData() |>
         tidytable::pull(SKELETON)
     )
