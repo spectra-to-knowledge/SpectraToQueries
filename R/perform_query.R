86de1afcbea8e8cdba8c80f8634e8de8a9b7adc6
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-18 18:54:38 +0100

    up

--- R/perform_query.R
+++ R/perform_query.R
@@ -9,31 +9,42 @@
 #' @return NULL
 #'
 #' @examples NULL
-perform_query <- function(spectra, frags, nls, dalton = 0.01, ppm = 25) {
-  if (length(frags) != 0) {
-    spectra <- spectra[spectra |>
-      Spectra::containsMz(
-        mz = frags,
-        tolerance = dalton,
-        ppm = ppm,
-        which = "all"
-      )]
+perform_query <- function(spectra,
+                          frags,
+                          nls,
+                          dalton = 0.01,
+                          ppm = 25) {
+  if (length(frags) > 0) {
+    spectra <- spectra[Spectra::containsMz(
+      spectra,
+      mz = frags,
+      tolerance = dalton,
+      ppm = ppm,
+      which = "all"
+    )]
   }
-  if (length(nls) != 0) {
-    if (length(spectra) != 0) {
-      # TODO report issue not taking "all" argument for now
-      for (nl in nls) {
-        if (length(spectra) != 0) {
-          spectra <- spectra[spectra |>
-            Spectra::containsNeutralLoss(
-              neutralLoss = nl,
-              tolerance = dalton,
-              ppm = ppm
-            )]
-        }
+
+  # Return early if spectra is empty after filtering
+  if (length(spectra) == 0) {
+    # message("Remaining spectra empty, very early return")
+    return(spectra)
+  }
+
+  if (length(nls) > 0) {
+    # TODO report issue not taking "all" argument for now
+    for (nl in nls) {
+      spectra <- spectra[Spectra::containsNeutralLoss(
+        spectra,
+        neutralLoss = nl,
+        tolerance = dalton,
+        ppm = ppm
+      )]
+      # Return early if spectra becomes empty during the loop
+      if (length(spectra) == 0) {
+        # message("Remaining spectra empty, early return")
+        return(spectra)
       }
     }
   }
-  # message("Query done.")
   return(spectra)
 }
