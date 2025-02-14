5c26e7e228d3e09d67e32980fee3c6400994a028
Adriano Rutz <adriano.rutz@hotmail.com>
2025-02-14 09:14:06 +0100

    refactor(globals.R): reduce warnings and add global variables to utils module

---
+++ R/globals.R
@@ -0,0 +1,19 @@
+utils::globalVariables(
+  c(
+    "SKELETON",
+    "count_per_ion",
+    "count_per_ion_per_group",
+    "fscore",
+    "group",
+    "group_count",
+    "id",
+    "ion",
+    "name",
+    "precision",
+    "recall",
+    "rowname",
+    "skeleton",
+    "target",
+    "value"
+  )
+)
