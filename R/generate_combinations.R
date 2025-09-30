0b83c584597bf9faf2413fc24492629b051096f1
Adriano Rutz <adriano.rutz@hotmail.com>
2025-09-30 09:27:38 +0200

    up

--- R/generate_combinations.R
+++ R/generate_combinations.R
@@ -27,34 +27,12 @@ generate_combinations <- function(x, max_ions) {
 #'
 #' @examples NULL
 generate_combinations_progress <- function(indices, ions_list, max_ions) {
-  n_indices <- length(indices)
-  use_parallel <- n_indices > 10L # Use parallel for larger sets
-
-  if (use_parallel) {
-    # Use parallel processing if beneficial
-    message(
-      "Using parallel processing for combination generation with ",
-      n_indices,
-      " groups."
-    )
-    BiocParallel::bplapply(
-      X = indices,
-      FUN = function(index) {
-        x <- ions_list[[index]]
-        generate_combinations(x = x, max_ions = max_ions)
-      },
-      BPPARAM = BiocParallel::bpparam(),
-      BPOPTIONS = BiocParallel::bpoptions(progressbar = TRUE)
-    )
-  } else {
-    # Fall back to sequential processing with progress
-    purrr::map(
-      .progress = TRUE,
-      .x = indices,
-      .f = function(index) {
-        x <- ions_list[[index]]
-        generate_combinations(x = x, max_ions = max_ions)
-      }
-    )
-  }
+  purrr::map(
+    .progress = TRUE,
+    .x = indices,
+    .f = function(index) {
+      x <- ions_list[[index]]
+      generate_combinations(x = x, max_ions = max_ions)
+    }
+  )
 }
